8cc7fb60f26b91d4b7d93834a46087e12e0d80b2
Brad Broge <brbroge@gmail.com>
2017-06-23 08:30:41 -0400

    wireframe css, starting functionality

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// work on things in vue
+
+let chatFunction = new Vue({
+  el: '.chat-box',
+  data: {
+    user-message = ""
+    // chatLog is a list of objects, with keys txt and isBot
+    chatLog = []
+  },
+  methods: {
+
+  }
+}
